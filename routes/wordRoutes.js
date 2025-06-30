7ac1c1a797c9c81d09e4ea8c360712ae84e412ea
Sumitgitup <sumitroy7august@gmail.com>
2025-06-30 16:18:59 +0530

    changed index.js

--- routes/wordRoutes.js
+++ routes/wordRoutes.js
@@ -1,16 +1,14 @@
-
-
-const  wordList = require('../wordlist.json')
 const express = require('express');
 const router = express.Router();
+const wordList = require('../wordlist.json');
 
+router.get('/', (req, res) => {
+  res.send('✅ Wordle API is live!');
+});
 
-router.get('/words',  (req, res ) => {
-
-    const words = wordList[(Math.floor(Math.random() * wordList.length))];
-    
-    res.json({words})
-
-})
+router.get('/words', (req, res) => {
+  const word = wordList[Math.floor(Math.random() * wordList.length)];
+  res.json({ word });
+});
 
 module.exports = router;
